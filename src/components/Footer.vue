f29449b07bfc6851a6bc6e57002b1a42590eaa28
Kemboi-Dun <gahrmicc@gmail.com>
2023-01-07 15:40:22 +0300

    edited the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,7 @@ const date = new Date().getFullYear();
 
 <template>
   <div class="footer_wrapper">
-    @{{ date }} Copyright.
-    <span> Designed and Developed 💻 by Paul Serian and Kemboi Duncan </span>
+    @{{ date }} Copyright | CodeBufferz
   </div>
 </template>
 <style scoped>
@@ -15,13 +14,6 @@ const date = new Date().getFullYear();
   color: var(--light-background);
   width: 100%;
   padding: 10px 32px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   font-size: 18px;
-}
-.footer_wrapper span {
-  color: var(--light-background);
-  background: transparent;
 }
 </style>
